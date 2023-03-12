e6b5179155a32214a9c8914815fc6355cfb44ac0
Asbjørn Dyhrberg Thegler <asbjoern@thegler.dk>
2023-03-12 15:36:05 +0100

    Update iOS handling

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
 import UAParser from 'ua-parser-js';
-
 import './style.css'
 
 const parser = new UAParser();
-
 const result = parser.getResult();
 const os = result.os;
 
@@ -17,18 +15,21 @@ let error;
 if (os.name === 'Android') {
   const version = Number(os.version);
 
-  if (os.version >= 6) ready = 'Your device is not ready.';
-  if (os.version >= 6) ready = 'Your device might be ready.';
-  if (os.version >= 9) ready = 'Your device is probably ready.';
-  if (os.version >= 11) ready = 'Your device is ready!';
+  ready = 'Your device is not ready for cell broadcast.';
+  if (version >= 4) ready = 'Your device might be ready for cell broadcast.';
+  if (version >= 9) ready = 'Your device is probably ready for cell broadcast.';
+  if (version >= 11) ready = 'Your device is ready for cell broadcast!';
 }
 
 if (os.name === 'iOS') {
-
+  const [major, minor] = os.version.split('.').map(Number);
+  ready = "You need to update your iPhone to the iOS 16 to be ready for cell broadcast."
+  if (major === 16) ready = "Your iPhone can soon be updated to be ready for cell broadcast."
+  if (minor >= 4) ready = "Your iPhone is ready for cell broadcast!"
 }
 
 if (!ready) {
-  error = "Your device was not recognized. Perhaps you are using a desktop device? Visit this site with your mobile device."
+  error = "Your device was not recognized. Are you using a computer? If so, visit this site with your mobile device."
 }
 
 document.querySelector('#content').innerHTML = `
@@ -40,12 +41,12 @@ document.querySelector('#content').innerHTML = `
       ${osString}
     </h1>
     ${ ready ? `
-    <h2>
+    <h2 id="success">
       ${ready}
     </h2>
     ` : ''}
     ${ error ? `
-    <h4>
+    <h4 id="error">
       ${error}
     </h4>
     ` : ''}
